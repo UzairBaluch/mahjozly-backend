e9fcaf4ca46dda979e4f7f03d8400c76643e47b1
Uzair Baluch <uzairbaluchdev@gmail.com>
2026-04-14 23:43:43 +0400

    Added GET /me route to auth.routes.ts for retrieving authenticated user information. Integrated authenticate middleware to ensure user is authenticated before accessing the route.

--- src/routes/v1/auth.routes.ts
+++ src/routes/v1/auth.routes.ts
@@ -2,11 +2,16 @@ import { validate } from '../../middlewares/validate.middleware.js';
 import { Router } from 'express';
 import { registerSchema, loginSchema } from '../../validations/auth.validation.js';
 import { registerUser, loginUser } from '../../controllers/auth.controller.js';
+import { authenticate } from '../../middlewares/auth.middleware.js';
 
 const authRouter = Router();
 
 // POST /api/v1/auth/register -> validate request body first, then run controller.
 authRouter.post('/register', validate(registerSchema), registerUser);
 authRouter.post('/login', validate(loginSchema), loginUser);
+// testing authenticate middleware test done 
+authRouter.get('/me', authenticate, async (req, res) => {
+  res.send(req.user);
+});
 
 export { authRouter };
